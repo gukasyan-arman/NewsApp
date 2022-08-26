65981d06b83acc8f8ee757dd9e2697838b0df44d
gukasyan-arman <gukasian.a.a@gmail.com>
2022-08-26 14:05:59 +0300

    adapter, recyclerView

--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -26,7 +26,6 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.all
     }
 
 }
