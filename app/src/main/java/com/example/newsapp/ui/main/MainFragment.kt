da3744bb96b472441af6cf01e44568106431ad3e
gukasyan-arman <gukasian.a.a@gmail.com>
2022-08-28 19:50:28 +0300

    viewModel, RecyclerView

--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -1,19 +1,27 @@
 package com.example.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.ui.adapters.NewsAdapter
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
+import retrofit2.Response
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
     lateinit var binding: FragmentMainBinding
 
     private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +33,34 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { response ->
+            when(response) {
+                is Resource.Success -> {
+                    progressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    progressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        Log.e("checkData", "MainFragment: error: ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    progressBar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
 
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        binding.mainRv.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
--- app/src/main/java/com/example/newsapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainViewModel.kt
@@ -1,11 +1,35 @@
 package com.example.newsapp.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.data.api.NewsRepository
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor(private val repository: NewsRepository): ViewModel() {
 
+    val newsLiveData: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var newsPage = 1
 
+    init {
+        getNews("ru")
+    }
+
+    private fun getNews(countryCode: String) =
+        viewModelScope.launch {
+            newsLiveData.postValue(Resource.Loading())
+            val response = repository.getNews(countryCode = countryCode, pageNumber = newsPage)
+            if (response.isSuccessful) {
+                response.body().let { res ->
+                    newsLiveData.postValue(Resource.Success(res))
+                }
+            } else {
+                newsLiveData.postValue((Resource.Error(message = response.message())))
+            }
+        }
 }
